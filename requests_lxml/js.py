7679fbbd0deb0afa48c2162c477b84e78ba87345
yuhongd <dongyuhong163@163.com>
2018-12-02 22:39:03 +0800

    input

---
+++ requests_lxml/js.py
@@ -0,0 +1,12 @@
+import re
+import execjs
+
+with open('tex.html') as f:
+    x = f.read()
+r = 'var carCompareJson = [[["124096","2.0T 手自一体 四驱 舒适版 5座 汽油","http://img1.bitautoimg.com/autoalbum/files/20180907/686/2018090716320132116676_2.jpg","3962","H9","哈弗H9","hafuh9","2017","在产","在销","","http://baa.bitauto.com/havalh9/","中大型SUV","炫晶黑,#000000|丛林绿,#3d512f|星空蓝,#424857|哈密尔顿灰,#555d66|匹兹堡银,#e2e2e2|汉密尔顿白,#ffffff"],["21.98万","18.98-21.98万","2.10万","3年或10万公里","2.0","涡轮增压","","8","手自一体","","8","8","2017","SUV","汽油"],["4856","1926","1900","2800","2330","5","","80","265/65 R17",]]]'
+reg = re.findall('carCompareJson = (.*)]]$',x)
+
+
+print(reg)
+print(len(reg))
+
